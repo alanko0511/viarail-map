4e950d4f3e28dbbb2867df4437c4fe3e28c2dd62
Alan Ko <34646302+alanko0511@users.noreply.github.com>
2026-04-04 09:26:53 -0400

    Remove all v1 files

--- src/theme/ThemeProvider.tsx
+++
@@ -1,9 +0,0 @@
-import { createTheme, MantineProvider } from "@mantine/core";
-import "@mantine/core/styles.css";
-import type { PropsWithChildren } from "react";
-
-const theme = createTheme({});
-
-export const ThemeProvider = (props: PropsWithChildren) => {
-  return <MantineProvider theme={theme}>{props.children}</MantineProvider>;
-};
